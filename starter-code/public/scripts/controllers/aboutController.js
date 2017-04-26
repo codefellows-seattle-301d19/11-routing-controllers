39da405faf45cabcc949cadba6ffe68af458e9e1
Adutchguy <mdm_8@hotmail.com>
2017-04-26 09:51:33 -0700

    So I did all the ToDo's in the starter code directory instead of the lynn-mike. So I went ahead and renamed the lynn-mike directory to starter code and renamed the starter code directory to lynn-mike.

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -3,10 +3,8 @@
 (function(module) {
   const aboutController = {};
 
-  aboutController.init = function() {
-    $('.tab-content').hide();
-    $('#about').show();
-  }
+  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+
 
   module.aboutController = aboutController;
 })(window);
