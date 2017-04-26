332dfb14cdd53302a4b14be166d5d63116fbf14d
Woojgh <jamessalamonsen@gmail.com>
2017-04-25 19:57:51 -0700

    Getting started

---
+++ james-brandon/public/scripts/controllers/routes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let outString = '';
+
+page('/', () => {
+  outString = `
+    
+  `;
+  $('main').html(outString);
+});
+// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// linked to a a single controller function to handle it:
+
+// TODO: What function do you call to activate page.js? Fire it off now, to execute
+page();
