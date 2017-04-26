ce80a545e64c11f7170635b7a61c5a9b2ed985bb
Zachary Diehl <zachary.diehl@gmail.com>
2017-04-26 10:57:52 -0700

    completed all TODO's, added article controller and removed mainNav handler function

--- zach-bret/public/scripts/controllers/routes.js
+++ zach-bret/public/scripts/controllers/routes.js
@@ -3,5 +3,6 @@
 // DONE: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 page('/about', aboutController.list);
+page('/', articleController.list);
 // DONE: What function do you call to activate page.js? Fire it off now, to execute
 page();
