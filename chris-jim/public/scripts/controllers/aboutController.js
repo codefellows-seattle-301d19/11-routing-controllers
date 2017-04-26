77f56f3af4cdbfcd639a6c15ecda5c9f6397a9bc
janousek77 <janousek77@gmail.com>
2017-04-26 10:06:23 -0700

    completed a few of the todos

--- chris-jim/public/scripts/controllers/aboutController.js
+++ chris-jim/public/scripts/controllers/aboutController.js
@@ -4,7 +4,13 @@
   const aboutController = {};
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
+  aboutController.show = function() {
+    $('.main-nav').on('click', '.tab', function() {
+      $('.tab-content').hide();
+      $(`#${$(this).data('content')}`).fadeIn();
+    });
+    $('.main-nav .tab:first').click();
+  };
 
   module.aboutController = aboutController;
 })(window);
--- chris-jim/public/scripts/controllers/routes.js
+++ chris-jim/public/scripts/controllers/routes.js
@@ -2,5 +2,8 @@
 
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
-
+page('/', articleController.show);
+page('/about', aboutController.show);
+page('/articles', articleController.show);
+page();
 // TODO: What function do you call to activate page.js? Fire it off now, to execute
