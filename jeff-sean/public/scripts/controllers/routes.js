193351b8b9cf754c80e301a4c2c1fb1bc716bf86
Sean Reigle <sereigle@gmail.com>
2017-04-25 17:43:50 -0700

    almost done

--- jeff-sean/public/scripts/controllers/routes.js
+++ jeff-sean/public/scripts/controllers/routes.js
@@ -4,3 +4,8 @@
 // linked to a a single controller function to handle it:
 
 // TODO: What function do you call to activate page.js? Fire it off now, to execute
+
+page('/', articleController.init)
+page('/about', aboutController.init)
+
+page();
